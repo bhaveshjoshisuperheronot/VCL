f599b6accf0cb2bd80b352a6b5d92f6d51b0ed8b
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2020-02-06 07:10:18 +0530

    First set

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AppContainer from './navigation/AppContainer'
+import Matches from './screens/Matches'
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <AppContainer />
+    );
+  };
+}
