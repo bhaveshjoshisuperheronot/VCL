81f6b0f08aba6b0641d07cee12cb8d62636238b2
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2020-02-09 11:03:15 +0530

    Update Team.js

--- screens/Team.js
+++ screens/Team.js
@@ -1,110 +1,113 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Constants from 'expo-constants';
 import Firebase from '../config/Firebase'
 
 var data_listA = [];
 var data_listB = [];
 var data_listFinal = [];
 
-function onPressHandler( id ){
-  Alert.alert(id);
-}
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
-function Item({ name, price, team, id }) {
+function Item({ id, name, selected, onSelect }) {
   return (
-    <View>
-      <TouchableOpacity style={styles.playerRow} onPress={() => onPressHandler(id)}>
-        <View><Text>{team}</Text></View>
-        <View><Text>{name}</Text></View>
-        <View><Text>{price}</Text></View>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity
+      onPress={() => onSelect(id)}
+      style={[
+        styles.item,
+        { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+      ]}
+    >
+      <Text style={styles.title}>{name}</Text>
+    </TouchableOpacity>
   );
 }
 
-export default class Team extends React.Component {
-    static navigationOptions = {
-        title: 'Team',
-        headerStyle: {
-          backgroundColor: '#ff7858',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        }
-    };
-    state = {
-      data: [],
-      dataA: [],
-      dataB: [],
-      dataFinal: [],
-    };
-    
-    componentDidMount(){
-      data_listA = []; data_listB = []; data_listFinal= [];
-        Firebase.database().ref('/players/').orderByChild('team').equalTo(this.props.navigation.state.params.teamA).on("value", function(snapshot) {
+export default function Team() {
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+    id => {
+      const newSelected = new Map(selected);
+      newSelected.set(id, !selected.get(id));
+
+      setSelected(newSelected);
+    },
+    [selected],
+  );
+
+  data_listA = []; data_listB = []; data_listFinal= [];
+        console.log(newFunction());
+        Firebase.database().ref('/players/').orderByChild('team').equalTo('FF').on("value", function(snapshot) {
           snapshot.forEach(function(data) {            
             data_listFinal.push( data.val() );
           });
           //data_listFinal.push( data_listA );
           //console.log(data_listA, data_listA.length); 
         });
-        Firebase.database().ref('/players/').orderByChild('team').equalTo(this.props.navigation.state.params.teamB).on("value", function(snapshot) {
+        Firebase.database().ref('/players/').orderByChild('team').equalTo('CG').on("value", function(snapshot) {
           snapshot.forEach(function(data) {            
             data_listFinal.push( data.val() );
           });
           //data_listFinal.push( data_listB );
-          console.log(data_listFinal, data_listFinal.length); 
+          //console.log(data_listFinal, data_listFinal.length);
         });
-      this.setState({ dataFinal : data_listFinal });  
-    }
-  render(){
-    return (
-        <View style={styles.container}>
-          <View></View>
-            <FlatList
-              data={data_listFinal}
-              renderItem={({ item }) => <Item name={item.name} price={item.price} team={item.team} id={item.key}/>}
-              keyExtractor={item => item.key}
-            />
-      </View>
-      
-    );
-  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data_listFinal}
+        renderItem={({ item }) => (
+          <Item
+            id={item.key}
+            name={item.name}
+            selected={!!selected.get(item.key)}
+            onSelect={onSelect}
+          />
+        )}
+        keyExtractor={item => item.key}
+        extraData={selected}
+      />
+    </SafeAreaView>
+  );
+
+  function newFunction() {
+    return () => this.props.navigation.state.params.teamA;
+  }
 }
+
 const styles = StyleSheet.create({
-    maincontainer: {
-    backgroundColor: '#fff',
-    borderColor: "#000",
-    borderWidth: 1,
-    borderRadius: 5,
-    width: "100%",
-    alignItems: "center",
-    height: 60,
-    justifyContent: "center",
-    marginBottom: 30
-  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 30,
-    paddingHorizontal: 10
+    marginTop: Constants.statusBarHeight,
   },
-  playerRow: {
-    backgroundColor: '#fff',
-    borderColor: "#000",
-    borderWidth: 1,
-    borderRadius: 5,
-    width: "100%",
-    alignItems: "center",
-    height: 50,
-    justifyContent: "center",
-    marginBottom: 10,
-    flexDirection:"row",
-    shadowColor: '#888',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.5,
-    shadowRadius: 2,
-    elevation: 1,
-  }
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
 });
