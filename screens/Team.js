723a1432d72d85f3fdf1c9618ff2a11e680297d8
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2020-02-10 20:04:12 +0530

    Update Team.js

--- screens/Team.js
+++ screens/Team.js
@@ -1,113 +1,209 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  TouchableOpacity,
-  FlatList,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import Constants from 'expo-constants';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity, Alert, Button } from 'react-native';
 import Firebase from '../config/Firebase'
 
 var data_listA = [];
 var data_listB = [];
 var data_listFinal = [];
+var player_index= [];
+var playerCount = 0;
+var team1Count = 0;
+var team2Count = 0;
+var teamPrice = 100;
+var playersSelected = 0;
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
-
-function Item({ id, name, selected, onSelect }) {
-  return (
-    <TouchableOpacity
-      onPress={() => onSelect(id)}
-      style={[
-        styles.item,
-        { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
-      ]}
-    >
-      <Text style={styles.title}>{name}</Text>
-    </TouchableOpacity>
-  );
+function onPressHandler( id ){
+  Alert.alert(id);
 }
 
-export default function Team() {
-  const [selected, setSelected] = React.useState(new Map());
 
-  const onSelect = React.useCallback(
-    id => {
-      const newSelected = new Map(selected);
-      newSelected.set(id, !selected.get(id));
 
-      setSelected(newSelected);
-    },
-    [selected],
-  );
-
-  data_listA = []; data_listB = []; data_listFinal= [];
-        console.log(newFunction());
-        Firebase.database().ref('/players/').orderByChild('team').equalTo('FF').on("value", function(snapshot) {
+export default class Team extends React.Component {
+    static navigationOptions = {
+        title: 'Team',
+        headerStyle: {
+          backgroundColor: '#ff7858',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        }
+    };
+    
+    constructor(props) {
+      super(props)
+      this.state = {
+        loading: false,
+        data: [],
+        dataA: [],
+        dataB: [],
+        dataFinal: [],
+        touchDisable: false,
+        buttonDisable: true,
+        teamprice: 100,
+        playersselected: 0,
+       };
+    }
+    
+    componentDidMount(){
+      data_listA = []; data_listB = []; data_listFinal= []; teamPrice = 100; playersSelected, team1Count, team2Count = 0;
+        Firebase.database().ref('/players/').orderByChild('team').equalTo(this.props.navigation.state.params.teamA).on("value", function(snapshot) {
           snapshot.forEach(function(data) {            
             data_listFinal.push( data.val() );
           });
           //data_listFinal.push( data_listA );
           //console.log(data_listA, data_listA.length); 
         });
-        Firebase.database().ref('/players/').orderByChild('team').equalTo('CG').on("value", function(snapshot) {
+        Firebase.database().ref('/players/').orderByChild('team').equalTo(this.props.navigation.state.params.teamB).on("value", function(snapshot) {
           snapshot.forEach(function(data) {            
             data_listFinal.push( data.val() );
           });
           //data_listFinal.push( data_listB );
-          //console.log(data_listFinal, data_listFinal.length);
+          //console.log(data_listFinal, data_listFinal.length); 
         });
+      this.setState({ dataFinal : data_listFinal });
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={data_listFinal}
-        renderItem={({ item }) => (
-          <Item
-            id={item.key}
-            name={item.name}
-            selected={!!selected.get(item.key)}
-            onSelect={onSelect}
-          />
-        )}
-        keyExtractor={item => item.key}
-        extraData={selected}
-      />
-    </SafeAreaView>
-  );
+      data_listFinal => data_listFinal.json();
+      data_listFinal = data_listFinal.map(item => {
+        item.isSelect = false;
+        item.selectedClass = styles.playerRow;
+        return item;
+      });
+      this.setState({ dataFinal : data_listFinal });
+    }
+    selectItem = data => {
+      data.item.isSelect = !data.item.isSelect;
+      data.item.selectedClass = data.item.isSelect ? styles.selected : styles.playerRow;
+      if(data.item.isSelect){
+        if(data.item.team == this.props.navigation.state.params.teamA){
+           team1Count++;
+        }
+        else{team2Count++;}
+        teamPrice = teamPrice - Number(data.item.price);
+        console.log(team1Count)
+        console.log(team2Count)
+        playersSelected = playersSelected + 1;
+        this.setState({ teamprice : teamPrice, playersselected : playersSelected });
+        if(playersSelected == 11 && teamPrice >= 0){
+          if(team1Count < 8 && team2Count < 8){this.setState({ buttonDisable: false })}
+          else{}
+        }
+        else{
+          this.setState({ buttonDisable: true })
+        }
+      }
+      else{
+        // if(data.item.team == this.props.navigation.state.params.teamA){team1Count--;}
+        // else{team2Count--;}
+        teamPrice = teamPrice + Number(data.item.price);
+        playersSelected = playersSelected - 1;
+        if(data.item.team == this.props.navigation.state.params.teamA){
+          team1Count--;
+        }
+        else{team2Count--;}
+        this.setState({ teamprice : teamPrice, playersselected : playersSelected });  
+        if(playersSelected == 11 && teamPrice >= 0){
+          if(team1Count < 8 && team2Count < 8){}
+          else{this.setState({ buttonDisable: false })}
+        }
+        else{
+          this.setState({ buttonDisable: true })
+        }      
+      }
 
-  function newFunction() {
-    return () => this.props.navigation.state.params.teamA;
-  }
+      const fg = this.state.dataFinal.findIndex(
+        item => data.item.team === item.team
+      );
+
+      //console.log(data_listFinal)
+
+      const index = this.state.dataFinal.findIndex(
+        item => data.item.key === item.key
+      );
+    
+      this.state.dataFinal[index] = data.item;
+      
+      this.setState({
+        dataFinal: this.state.dataFinal,
+      });
+      
+    };
+    renderItem = data =>       
+        <View>
+          <TouchableOpacity 
+            style={[styles.playerRow, data.item.selectedClass]}
+            onPress={() => this.selectItem(data)}
+            disabled={this.state.touchDisable}
+          >
+            
+            <View><Text>{data.item.team}</Text></View>
+            <View><Text>{data.item.team}</Text></View>
+            <View><Text>{data.item.name}</Text></View>
+            <View><Text>{data.item.price}</Text></View>
+            
+          </TouchableOpacity>
+        </View>
+      
+  render(){
+    return (
+        <View style={styles.container}>
+          
+          <View>
+            <Text>Price Remaining: {this.state.teamprice} credits</Text>
+            <Text>Players Selected : {this.state.playersselected} player(s) selected</Text>
+            <Text>Players {this.props.navigation.state.params.teamA}: {team1Count} (max 7 players allowed)</Text>
+            <Text>Players {this.props.navigation.state.params.teamB}: {team2Count} (max 7 players allowed)</Text>
+          </View>
+          <View><Button 
+            title="Save Team"
+            disabled={this.state.buttonDisable}
+          /></View>
+          <View></View>
+            <FlatList
+              data={data_listFinal}
+              renderItem={item => this.renderItem(item)}
+              keyExtractor={item => item.key.toString()}
+              extraData={this.state}
+            />
+      </View>
+      
+    );
+  };
 }
-
 const styles = StyleSheet.create({
+    maincontainer: {
+    backgroundColor: '#fff',
+    borderColor: "#000",
+    borderWidth: 1,
+    borderRadius: 5,
+    width: "100%",
+    alignItems: "center",
+    height: 60,
+    justifyContent: "center",
+    marginBottom: 30
+  },
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
+    backgroundColor: '#fff',
+    paddingTop: 30,
+    paddingHorizontal: 10
   },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+  playerRow: {
+    backgroundColor: '#fff',
+    borderColor: "#000",
+    borderWidth: 1,
+    borderRadius: 5,
+    width: "100%",
+    alignItems: "center",
+    height: 50,
+    justifyContent: "center",
+    marginBottom: 10,
+    flexDirection:"row",
+    shadowColor: '#888',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 1,
   },
-  title: {
-    fontSize: 32,
-  },
+  selected: {backgroundColor: "#3DDC84", borderColor: "#3DDC84",},
 });
