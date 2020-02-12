9013752ef8bb92b33e1dc7d1a543111dded58b89
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2020-02-12 22:20:33 +0530

    Add files via upload

--- AppNavigation.js
+++ AppNavigation.js
@@ -1,11 +1,16 @@
 import { createStackNavigator } from 'react-navigation-stack'
 import Matches from '../screens/Matches'
 import Team from '../screens/Team'
+import Points from "../screens/Scores"
+import Leaderboard from "../screens/Leaderboard"
+
 
 const AppNavigation = createStackNavigator(
   {
     Matches: { screen: Matches },
-    Team: { screen: Team}
+    Team: { screen: Team},
+    Points: { screen: Points},
+    Leaderboard: { screen: Leaderboard},
   },
   {
     initialRouteName: 'Matches'
