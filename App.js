ea465ef530d8446b5cb496a55dcf70c9386078ec
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2020-02-12 22:21:30 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import AppContainer from './navigation/AppContainer'
-import Matches from './screens/Matches'
+import AppContainer from './navigation'
 
 export default class App extends React.Component {
   render(){
